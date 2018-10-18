40375b2d25c075514f193876bbb25947e2b13f90
REDANT <redant.benjamin@gmail.com>
2018-10-18 10:32:46 +0200

    Add scan

--- src/app/scan/scan.module.ts
+++ src/app/scan/scan.module.ts
@@ -1,26 +1,32 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { ScanPage } from './scan.page';
+import {ScanPage} from './scan.page';
+import {NgProgressModule} from '@ngx-progressbar/core';
+import {Camera} from '@ionic-native/camera/ngx';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ScanPage
-  }
+    {
+        path: '',
+        component: ScanPage
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [ScanPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        NgProgressModule,
+        RouterModule.forChild(routes),
+    ],
+    declarations: [ScanPage],
+    providers: [Camera]
 })
-export class ScanPageModule {}
+
+export class ScanPageModule {
+}
