496f695ced6e605c679a7a7fc744901893f00717
LITTIERE <jeremie.littiere@gmail.com>
2018-10-19 15:29:34 +0200

    feat(modal add element): Display modal Add Element

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,7 +21,8 @@ const routes: Routes = [
   { path: 'historic', loadChildren: './historic/historic.module#HistoricPageModule' },
   { path: 'debt', loadChildren: './debt/debt.module#DebtPageModule' },
   { path: 'bill', loadChildren: './bill/bill.module#BillPageModule' },
-  { path: 'validate-bill', loadChildren: './validate-bill/validate-bill.module#ValidateBillPageModule' }
+  { path: 'validate-bill', loadChildren: './validate-bill/validate-bill.module#ValidateBillPageModule' },
+  { path: 'add', loadChildren: './validate-bill/add/add.module#AddPageModule' }
 
 
 ];
