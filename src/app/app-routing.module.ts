77adfb313717313c2764f9106cbeaa41f2512127
LITTIERE <jeremie.littiere@gmail.com>
2018-10-19 15:29:34 +0200

    feat(modal add element): Display modal Add Element

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,13 +16,12 @@ const routes: Routes = [
     loadChildren: './list/list.module#ListPageModule'
   },
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
-  { path: 'scan', loadChildren: './scan/scan.module#ScanPageModule' },  { path: 'join', loadChildren: './join/join.module#JoinPageModule' },
+  { path: 'scan', loadChildren: './scan/scan.module#ScanPageModule' },
+  { path: 'join', loadChildren: './join/join.module#JoinPageModule' },
   { path: 'historic', loadChildren: './historic/historic.module#HistoricPageModule' },
   { path: 'debt', loadChildren: './debt/debt.module#DebtPageModule' },
   { path: 'bill', loadChildren: './bill/bill.module#BillPageModule' },
-  { path: 'validate-bill', loadChildren: './validate-bill/validate-bill.module#ValidateBillPageModule' },
-  { path: 'add', loadChildren: './validate-bill/add/add.module#AddPageModule' },
-  { path: 'add', loadChildren: './validate-bill/add/add.module#AddPageModule' }
+  { path: 'validate-bill', loadChildren: './validate-bill/validate-bill.module#ValidateBillPageModule' }
 
 
 ];
--- src/app/validate-bill/validate-bill.module.ts
+++ src/app/validate-bill/validate-bill.module.ts
@@ -6,43 +6,23 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ValidateBillPage } from './validate-bill.page';
-import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { ModalPage } from './add/add.page';
+import {AddPage} from './add/add.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ValidateBillPage
-  }
+    {
+        path: '',
+        component: ValidateBillPage
+    }
 ];
 
-@Component({
-    selector: 'validate-bill',
-    templateUrl: 'validate-bill.page.html',
-    styleUrls: ['./validate-bill.page.css']
-})
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [ValidateBillPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes)
+    ],
+    declarations: [ValidateBillPage, AddPage],
+    entryComponents: [AddPage]
 })
-export class ValidateBillPageModule {
-
-    constructor(public modalController: ModalController) {}
-
-    async openModalAdd() {
-        const modal = await this.modalController.create({
-            component: ModalPage,
-            componentProps: { value: 123 }
-        });
-        return await modal.present();
-    }
-
-
-}
+export class ValidateBillPageModule {}
