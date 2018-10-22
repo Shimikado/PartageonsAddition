04ea8290fc5ce7bb416581a4b55729cbd541edff
REDANT <redant.benjamin@gmail.com>
2018-10-22 21:11:18 +0200

    init firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase/app';
 
 @Component({
     selector: 'app-root',
@@ -43,5 +44,15 @@ export class AppComponent {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
         });
+        // Config for firebase
+        const configFirebase = {
+            apiKey: 'AIzaSyCNK9vo3mI3mxN_AqMoZmsxt5wjPIz3zro',
+            authDomain: 'partageons-l-addition.firebaseapp.com',
+            databaseURL: 'https://partageons-l-addition.firebaseio.com',
+            projectId: 'partageons-l-addition',
+            storageBucket: 'partageons-l-addition.appspot.com',
+            messagingSenderId: '389644409346'
+        };
+        firebase.initializeApp(configFirebase);
     }
 }
