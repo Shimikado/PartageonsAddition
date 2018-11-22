2fd15489972c7e53509784f27e506521f92ff5e5
REDANT <redant.benjamin@gmail.com>
2018-11-22 15:57:12 +0100

    feat(validation): create parsing

--- src/app/validate-bill/validate-bill.page.ts
+++ src/app/validate-bill/validate-bill.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 import {ModalController} from '@ionic/angular';
 import {AddPage} from './add/add.page';
+import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
@@ -12,9 +13,44 @@ import {AddPage} from './add/add.page';
 export class ValidateBillPage implements OnInit {
 
     public listItem: any[];
+    private inputText: string;
+    private sum: number;
 
-    constructor(public modalController: ModalController) {
-        this.listItem = [{name: 'Matrimonio', quantity: 1, value: 1}];
+    constructor(public modalController: ModalController,
+                private activeRoute: ActivatedRoute) {
+        this.listItem = [];
+        this.sum = 0;
+        this.activeRoute.queryParams.subscribe(data => {
+            this.inputText = data.inputText;
+            this.billParser();
+        });
+    }
+
+    ngOnInit() {
+    }
+
+    private billParser() {
+        this.listItem = [];
+        this.sum = 0;
+        const currentList = this.inputText.match(/[0-9]+[a-zA-Z ]+[0-9 ]+[\., ]*[0-9 ]*€/g);
+        currentList.forEach(
+            element => {
+                const prixAvecSymbole = element.match(/[0-9 ]+[\., ]*[0-9 ]*€/g)[0];
+                const prix = prixAvecSymbole.substr(0, prixAvecSymbole.length - 1);
+                const label = element.match(/[a-zA-Z ]+/)[0];
+                const quantity = element.match(/[0-9]+/)[0];
+                const devise = element.match(/€/)[0];
+                const product = {
+                    label: label.trim(),
+                    prix: prix.trim(),
+                    devise: devise.trim(),
+                    quantity: quantity.trim(),
+                    nom: 'Jean mich mich'.trim(),
+                };
+                this.sum += +product.prix;
+                this.listItem.push(product);
+            }
+        );
     }
 
     async openModalAdd() {
@@ -27,9 +63,6 @@ export class ValidateBillPage implements OnInit {
                 this.listItem.push(data);
             });
         return await modal.present();
-    }
-
-    ngOnInit() {
     }
 
 }
